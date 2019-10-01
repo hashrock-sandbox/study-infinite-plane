e714d0b97fbe37af9cb81926d409cda6aafea0d9
hashrock <hashrock@users.noreply.github.com>
2019-10-01 23:21:05 +0900

    feat: make it zoomable

--- App.vue
+++ App.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <svg
+      ref="canvas"
       viewBox="0 0 300 300"
       width="300"
       height="300"
@@ -8,8 +9,11 @@
       @pointermove="onPointerMove"
       @pointerup="onPointerUp"
     >
-      <circle v-for="p in points" :cx="p.x" :cy="p.y" :key="p.id" r="10" />
+      <circle v-for="p in points" :cx="p.x" :cy="p.y" :key="p.id" :r="10 * viewport.zoom" />
     </svg>
+    <div>
+      <input type="range" v-model.number="viewport.zoom" min="0.1" max="2" step="0.05" />
+    </div>
   </div>
 </template>
 <script>
@@ -19,8 +23,8 @@ export default {
       return this.src.map(p => {
         return {
           ...p,
-          x: p.x + this.viewport.x,
-          y: p.y + this.viewport.y
+          x: (p.x + this.viewport.x) * this.viewport.zoom,
+          y: (p.y + this.viewport.y) * this.viewport.zoom
         };
       });
     }
@@ -28,14 +32,16 @@ export default {
   methods: {
     onPointerMove(ev) {
       if (this.grab) {
-        this.viewport.x = ev.offsetX - this.grab.x;
-        this.viewport.y = ev.offsetY - this.grab.y;
+        this.viewport.x = ev.offsetX / this.viewport.zoom - this.grab.x;
+        this.viewport.y = ev.offsetY / this.viewport.zoom - this.grab.y;
       }
     },
     onPointerDown(ev) {
+      this.$refs.canvas.setPointerCapture(ev.pointerId);
+
       this.grab = {
-        x: ev.offsetX - this.viewport.x,
-        y: ev.offsetY - this.viewport.y
+        x: ev.offsetX / this.viewport.zoom - this.viewport.x,
+        y: ev.offsetY / this.viewport.zoom - this.viewport.y
       };
     },
     onPointerUp() {
@@ -51,7 +57,17 @@ export default {
           x: 10,
           y: 10
         },
-        { id: 1, x: 20, y: 30 }
+        { id: 1, x: 20, y: 30 },
+        {
+          id: 2,
+          x: 50,
+          y: 30
+        },
+        {
+          id: 3,
+          x: 80,
+          y: 90
+        }
       ],
       viewport: {
         x: 0,
@@ -68,5 +84,15 @@ body {
 }
 svg {
   background: white;
+  cursor: grabbing;
+}
+circle {
+  transition: fill 0.1s;
+  fill: rebeccapurple;
+  cursor: pointer;
+}
+circle:hover {
+  stroke: rebeccapurple;
+  fill: white;
 }
 </style>
